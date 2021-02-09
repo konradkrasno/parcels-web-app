32369da3cb81ec98bafab35c313b9f7f99ee75e8
konradkrasno <konradkrasno@gmail.com>
2021-02-09 19:28:03 +0100

    add allowed host

--- parcels_web_app/settings.py
+++ parcels_web_app/settings.py
@@ -29,9 +29,9 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["parcels-web-app.herokuapp.com"]
 
 
 # Application definition
